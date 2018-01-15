9a55737a1cc464346c8cd531205534ee9f33015d
doriandekoning <doriandekoning@gmail.com>
2018-01-15 22:20:08 +0100

    Cleanup create UI

--- android/app/src/main/java/org/blockchainbeasts/passbuddies/RecoverSecretActivity.java
+++ android/app/src/main/java/org/blockchainbeasts/passbuddies/RecoverSecretActivity.java
@@ -55,15 +55,6 @@ public class RecoverSecretActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        //TODO ReMOVE butotn
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     private void handleNfcIntent(Intent NfcIntent) {
