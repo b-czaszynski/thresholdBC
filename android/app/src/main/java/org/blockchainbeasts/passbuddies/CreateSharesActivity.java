c0a1d388570ca5e8d43c108d9c551f3f882bfa65
doriandekoning <doriandekoning@gmail.com>
2018-01-20 14:07:30 +0100

    Add amount to keep input field

--- android/app/src/main/java/org/blockchainbeasts/passbuddies/CreateSharesActivity.java
+++ android/app/src/main/java/org/blockchainbeasts/passbuddies/CreateSharesActivity.java
@@ -1,8 +1,6 @@
 package org.blockchainbeasts.passbuddies;
 
 import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
@@ -18,7 +16,6 @@ import android.widget.TextView;
 import com.codahale.shamir.Scheme;
 
 import org.json.JSONException;
-import org.w3c.dom.Text;
 
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
@@ -28,9 +25,13 @@ public class CreateSharesActivity extends AppCompatActivity implements NfcAdapte
         NfcAdapter.CreateNdefMessageCallback{
 
     private ArrayList<Secret> messagesToSendArray;
-    private int succesfullSent = 0;
+    private int successfulSent = 0;
     private int n;
 
+    /**
+     * Called when this Activity is created
+     * @param savedInstanceState state to be used to restore from a previous state
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,35 +45,45 @@ public class CreateSharesActivity extends AppCompatActivity implements NfcAdapte
         messagesToSendArray = new ArrayList<>();
     }
 
+    /**
+     * Creates shares according to the given inputs
+     * @param view the view  that calls this method
+     */
     public void createShares(View view) {
-        succesfullSent = 0;
-        //TODO input validation
+        successfulSent = 0;
+        // Get info from input fields
         byte[] secret =  ((EditText)findViewById(R.id.txtBoxSecret)).getText().toString().getBytes(StandardCharsets.UTF_8);
         String name = ((EditText)findViewById(R.id.txtBoxSecretName)).getText().toString();
-        String userName = this.getSharedPreferences("username", Context.MODE_PRIVATE).getString("username", null);
+        String username = this.getSharedPreferences("username", Context.MODE_PRIVATE).getString("username", null);
         n = Integer.parseInt(((EditText)findViewById(R.id.numberInputN)).getText().toString());
         int k = Integer.parseInt(((EditText)findViewById(R.id.numberInputK)).getText().toString());
+        int amountToKeep = Integer.parseInt(((EditText)findViewById(R.id.numberInputAmountToKeep)).getText().toString());
+
         Scheme scheme = new Scheme(n, k);
         Map<Integer, byte[]> sharesBytes = scheme.split(secret);
         ArrayList<Share> shares = new ArrayList<>();
         for(int key : sharesBytes.keySet()){
             shares.add(new Share(sharesBytes.get(key), key));
         }
-        if(shares.get(0) != null){
-            try {
-                Secret s = new Secret(userName, name, n, k);
-                s.addShare(shares.remove(0));
-                succesfullSent++;
-                SecretStorageHandler.storeSecret(this, s);
-            }catch(JSONException e){
-                e.printStackTrace();
+        // Keep shares self
+        for(int i = 0; i < amountToKeep; i++) {
+            if (shares.get(0) != null) {
+                try {
+                    Secret s = new Secret(username, name, n, k);
+                    s.addShare(shares.remove(0));
+                    successfulSent++;
+                    SecretStorageHandler.storeSecret(this, s);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         }
         while(!shares.isEmpty()){
-            Secret s = new Secret("", name);
+            Secret s = new Secret(username, name);
             s.addShare(shares.remove(0));
             messagesToSendArray.add(s);
         }
+        // Set progress
         RelativeLayout relativeLayout = findViewById(R.id.relativeLayoutSecretInput);
         relativeLayout.setVisibility(View.GONE);
         TextView progressView = findViewById(R.id.txtViewProgress);
@@ -80,47 +91,53 @@ public class CreateSharesActivity extends AppCompatActivity implements NfcAdapte
         // Register callback for nfc (i.e. enable nfcf)
         NfcAdapter.getDefaultAdapter(this).setNdefPushMessageCallback(this, this);
         NfcAdapter.getDefaultAdapter(this).setOnNdefPushCompleteCallback(this, this);
-
-//TODO give user feedback about the sending process
-        //startActivity(new Intent(this, CreateSharesActivity.class));
     }
 
-    public NdefRecord[] createRecords() {
-
+    /**
+     * Creates a list of NdefRecords to be sent
+     * @return a list of messages to be sent
+     */
+    public NdefRecord[] createRecords() throws JSONException {
         NdefRecord[] records = new NdefRecord[2];
-        byte[] payload;
-        try {
-            payload = messagesToSendArray.remove(0).toJSON().getBytes(StandardCharsets.UTF_8);
-        }catch(JSONException e) {
-            e.printStackTrace();
-            return new NdefRecord[0];
-        }
-        System.out.println("Sending: " + new String(payload, StandardCharsets.UTF_8));
-        records[0] =  NdefRecord.createMime("text/plain",payload);
+        byte[] payload = messagesToSendArray.remove(0).toJSON().getBytes(StandardCharsets.UTF_8);
+        records[0] =  NdefRecord.createMime("text/plain", payload);
         records[1] = NdefRecord.createApplicationRecord(this.getPackageName());
         return records;
     }
 
+    /**
+     * Creates a single NDF message (called when phone gets close to another nfc enabled device)
+     * @param event nfc event containing information about nfc
+     * @return returns the next message to be sent
+     */
     @Override
     public NdefMessage createNdefMessage(NfcEvent event) {
         if(messagesToSendArray.size()>0) {
-            return new NdefMessage( createRecords());
+            try {
+                return new NdefMessage( createRecords());
+            } catch (JSONException e) {
+                e.printStackTrace();
+                return null;
+            }
         }else{
             finish();
             return null;
         }
     }
 
-
+    /**
+     * Gets called when an nfc message is succesfully send to another device.
+     * @param nfcEvent an nfcEvent object containing data about delivering a message it to the other device
+     */
     @Override
     public void onNdefPushComplete(NfcEvent nfcEvent) {
-        succesfullSent++;
+        successfulSent++;
         //Make sure the text is set on the main thread
         runOnUiThread(() -> {
-            if(succesfullSent<n) {
+            if(successfulSent <n) {
                 TextView progressView = findViewById(R.id.txtViewProgress);
-                progressView.setText("Successfully shared with " + succesfullSent + " friends, share it with " + (n - succesfullSent) + " more");
-            }else{
+                progressView.setText("Successfully shared with " + successfulSent + " friends, share it with " + (n - successfulSent) + " more");
+            }else{ // All messages are send
                 TextView progressView = findViewById(R.id.txtViewProgress);
                 progressView.setText("");
                 RelativeLayout relativeLayout = findViewById(R.id.relativeLayoutSecretInput);
@@ -130,10 +147,14 @@ public class CreateSharesActivity extends AppCompatActivity implements NfcAdapte
         });
     }
 
+    /**
+     * Removes any values from inputs
+     */
     private void clearInputs() {
         ((EditText)findViewById(R.id.txtBoxSecret)).setText("");
         ((EditText)findViewById(R.id.txtBoxSecretName)).setText("");
         ((EditText)findViewById(R.id.numberInputN)).setText("");
         ((EditText)findViewById(R.id.numberInputK)).setText("");
+        ((EditText)findViewById(R.id.numberInputAmountToKeep)).setText("");
     }
 }
